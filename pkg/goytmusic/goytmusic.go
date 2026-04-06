7c0e1f7091aca46e6de53e43c75c1d7f51c7c07e
Maicon Leles <maiconnpml@gmail.com>
2026-04-05 22:06:28 -0300

    fix(goytmusic): Fix user agent and construction of SAPISIDHASH

--- pkg/goytmusic/goytmusic.go
+++ pkg/goytmusic/goytmusic.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	defaultBaseURL   = "https://music.youtube.com/youtubei/v1/"
-	defaultUserAgent = "go-ytm/0.0.0"
+	defaultUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
 	defaultOrigin    = "https://music.youtube.com"
 )
 
@@ -147,6 +147,9 @@ func sapisidFromCookie(cookie string) string {
 	for part := range parts {
 		part = strings.TrimSpace(part)
 		if s, found := strings.CutPrefix(part, "__Secure-3PAPISID="); found {
+			return s
+		}
+		if s, found := strings.CutPrefix(part, "SAPISID="); found {
 			return s
 		}
 	}
